015d8148178a4a36c824cc1b54d198d162bddf43
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-12-14 08:55:33 +0000

    Worked on demos

--- examples/simple_publisher.py
+++ examples/simple_publisher.py
@@ -1,10 +1,13 @@
 """Simple Subscriber"""
 
 import asyncio
+import logging
 
 from aioconsole import ainput, aprint
+import click
 
 from squawkbus import SquawkbusClient, DataPacket
+
 
 async def get_message() -> tuple[str, list[DataPacket]]:
     data_packets: list[DataPacket] = []
@@ -21,9 +24,9 @@ async def get_message() -> tuple[str, list[DataPacket]]:
         content_type = await ainput("Content type (text/plain): ")
         if content_type == '':
             content_type = 'text/plain'
-        
+
         data = await ainput("Data: ")
-        
+
         packet = DataPacket(
             int(entitlement),
             content_type,
@@ -33,32 +36,31 @@ async def get_message() -> tuple[str, list[DataPacket]]:
 
     return topic, data_packets
 
-async def main() -> None:
-    await aprint('Example publisher')
+
+async def main_async(host: str, port: int):
 
     client = await SquawkbusClient.create('localhost', 8558)
 
     while True:
-        topic = await ainput('Topic: ')
-        if topic == '':
+        await aprint("Enter a new message")
+        topic, data_packets = await get_message()
+        if not topic:
             break
-        data_packets: list[DataPacket] = []
-        while True:
-            entitlement = await ainput("Entitlement (<ENTER> to stop): ")
-            if entitlement == '':
-                break
-            content_type = await ainput("Content type (text/plain): ")
-            if content_type == '':
-                content_type = 'text/plain'
-            data = await ainput("Data: ")
-            packet = DataPacket(int(entitlement), content_type, data.encode('utf-8'))
-            data_packets.append(packet)
 
-            await aprint(f"Sending to {topic} data {data_packets}")
-            await client.publish(topic, data_packets)
-    
+        await client.publish(topic, data_packets)
+
     client.close()
     await client.wait_closed()
 
+
+@click.command()
+@click.option("-h", "--host", "host", type=str, default="localhost")
+@click.option("-p", "--port", "port", type=int, default=8558)
+def main(host: str, port: int) -> None:
+    logging.basicConfig(level=logging.ERROR)
+    asyncio.run(main_async(host, port))
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    # pylint: disable=no-value-for-parameter
+    main()
--- examples/simple_subscriber.py
+++ examples/simple_subscriber.py
@@ -3,6 +3,8 @@
 import asyncio
 import logging
 
+import click
+
 from squawkbus import SquawkbusClient, DataPacket
 
 LOG = logging.getLogger("subscriber")
@@ -17,16 +19,25 @@ async def on_data(
     print(f'sender="{sender}",host="{host}",topic="{topic}",data={data}')
 
 
-async def main():
+async def main_async(host: str, port: int):
     topic = input('Topic: ')
 
-    client = await SquawkbusClient.create('localhost', 8558)
+    client = await SquawkbusClient.create(host, port)
     client.data_handlers.append(on_data)
 
     await client.add_subscription(topic)
 
     await client.wait_closed()
 
-if __name__ == '__main__':
+
+@click.command()
+@click.option("-h", "--host", "host", type=str, default="localhost")
+@click.option("-p", "--port", "port", type=int, default=8558)
+def main(host: str, port: int) -> None:
     logging.basicConfig(level=logging.ERROR)
-    asyncio.run(main())
+    asyncio.run(main_async(host, port))
+
+
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    main()
