e1227ffdd642b7ead479f9a0345d6f01d3cc0c5a
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-12-10 07:18:02 +0000

    Made data packet entitlements a set

--- src/squawkbus/data_packet.py
+++ src/squawkbus/data_packet.py
@@ -6,31 +6,31 @@ class DataPacket:
 
     def __init__(
             self,
-            entitlement: int,
+            entitlements: set[int],
             content_type: str,
             data: bytes
     ) -> None:
         """Initialise a data packet.
 
         Args:
-            entitlement (int): The required packet entitlement.
+            entitlements (set[int]): The required packet entitlements.
             content_type (str): The content type of the data.
             data (bytes): The data.
         """
-        self.entitlement = entitlement
+        self.entitlements = entitlements
         self.content_type = content_type
         self.data = data
 
     def __str__(self) -> str:
-        return f'entitlement={self.entitlement},content_type={self.content_type!r}, data={self.data!r}'
+        return f'{self.entitlements=},{self.content_type=},{self.data=}'
 
     def __repr__(self):
-        return f'DataPacket({self.entitlement}, {self.content_type!r}, {self.data!r})'
+        return f'DataPacket({self.entitlements!r},{self.content_type!r},{self.data!r})'
 
     def __eq__(self, value):
         return (
             isinstance(value, DataPacket) and
-            self.entitlement == value.entitlement and
+            self.entitlements == value.entitlements and
             self.content_type == value.content_type and
             self.data == value.data
         )
